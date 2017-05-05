0450f018e836d3c4279cfc720edfc3b3acf2e553
Tim <dev@tim.ph>
2017-05-04 23:33:31 -0400

    adding registration utility

---
+++ src/Util/Registration/Plugin.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * BoldGrid Library Plugin Registration Utility
+ *
+ * @package Boldgrid\Library
+ * @subpackage \Util\Registration
+ *
+ * @version 1.0.0
+ * @author BoldGrid <wpb@boldgrid.com>
+ */
+
+namespace Boldgrid\Library\Util\Registration;
+
+use Boldgrid\Library\Util;
+
+/**
+ * BoldGrid Library Plugin Registration Class.
+ *
+ * This class is responsible for handling the registration of
+ * a plugin and it's associated dependency version.
+ *
+ * @since 1.0.0
+ */
+class Plugin extends AbstractRegistration {
+
+	/**
+	 * Initialize class and set class properties.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $product The path of the product.
+	 */
+	public function __construct( $product, $dependency = 'boldgrid/library' ) {
+		Util\Option::init();
+		$this->product = plugin_basename( $product );
+		$this->dependency = $dependency;
+		register_activation_hook( $this->getProduct(), array( $this, 'register' ) );
+		register_deactivation_hook( $this->getProduct(), array( $this, 'deregister' ) );
+	}
+}
---
+++ src/Util/Registration/RegistrationInterface.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * BoldGrid Library Registration Utility Interface
+ *
+ * @package Boldgrid\Library
+ * @subpackage \Util\Registration
+ *
+ * @version 1.0.0
+ * @author BoldGrid <wpb@boldgrid.com>
+ */
+
+namespace Boldgrid\Library\Util\Registration;
+
+/**
+ * BoldGrid Library Registration Interface
+ *
+ * This interface dictates how classes that implement it
+ * should be registering and deregistering products.
+ */
+interface RegistrationInterface {
+
+	/**
+	 * Initialize the class and set class properties.
+	 */
+	public function __construct( $product, $dependency );
+
+	/**
+	 * Register the product in WordPress options.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @return null
+	 */
+	public function register();
+
+	/**
+	 * Deregister the product in WordPress options.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @return null
+	 */
+	public function deregister();
+
+	/**
+	 * Gets the product class property.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @return string $product Product class property.
+	 */
+	public function getProduct();
+
+	/**
+	 * Get the dependency class property.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @return string $dependency The dependency class property.
+	 */
+	public function getDependency();
+}
