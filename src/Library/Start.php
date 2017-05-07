7f843665addded2ba225ab858df40f7aeb4e55ef
Tim <dev@tim.ph>
2017-05-07 16:31:39 -0400

    adding init class for lib

---
+++ src/Library/Start.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * BoldGrid Library Start.
+ *
+ * @package Boldgrid\Library
+ * @subpackage \Library
+ *
+ * @version 1.0.0
+ * @author BoldGrid <wpb@boldgrid.com>
+ */
+
+namespace Boldgrid\Library\Library;
+
+/**
+ * BoldGrid Library Start Class.
+ *
+ * This class is responsible for setting up the BoldGrid Library.
+ *
+ * @since 1.0.0
+ */
+class Start {
+
+	/**
+	 * @access private
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var object $key Library Key Object.
+	 */
+	private
+		$key;
+
+
+	/**
+	 * Initialize class and set class properties.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $configs Plugin configuration array.
+	 */
+	public function __construct( $configs = null ) {
+		new Configs( $configs );
+		$this->key = new Key();
+	}
+}
--- src/Library/Views/License.php
+++
@@ -1,5 +0,0 @@
-<div class="notice notice-warning is-dismissible">
-	<p>
-		<?php esc_html( 'You must have a valid BoldGrid Premium license in order to use premium plugins.', 'boldgrid-inspirations' ); ?>
-	</p>
-</div>
