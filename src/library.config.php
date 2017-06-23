30013ceb9aab71021caa74d56b4f2b666799b279
Tim <dev@tim.ph>
2017-06-22 23:09:37 -0400

    adding library plugin installer configs

--- src/library.config.php
+++ src/library.config.php
@@ -15,6 +15,49 @@ return array(
 	'main_file_path' => "$slug/$slug.php",
 	'key' => get_site_option( 'boldgrid_api_key', null ),
 	'apiData' => get_site_transient( 'boldgrid_api_data' ),
+
+	// Enable key validation in library.
 	'keyValidate' => true,
+
+	// Enable license activation/deactivation in library.
 	'licenseActivate' => true,
+
+	// Library's Plugin Installer for "Plugins > Add New" in WordPress Dashboard.
+	'pluginInstaller' => array(
+
+		// Enabled the plugin installer feature in library.
+		'enabled' => true,
+
+		// Installable plugins.
+		'plugins' => array(
+			'boldgrid-inspirations' => array(
+				'key' => 'core',
+				'file' => 'boldgrid-inspirations/boldgrid-inspirations.php',
+			),
+			'boldgrid-staging' => array(
+				'key' => 'staging',
+				'file' => 'boldgrid-staging/boldgrid-staging.php',
+			),
+			'boldgrid-seo' => array(
+				'key' => 'seo',
+				'file' => 'boldgrid-seo/boldgrid-seo.php',
+			),
+			'boldgrid-editor' => array(
+				'key' => 'editor',
+				'file' => 'boldgrid-editor/boldgrid-editor.php',
+			),
+			'boldgrid-backup' => array(
+				'key' => 'backup',
+				'file' => 'boldgrid-backup/boldgrid-backup.php',
+			),
+			'boldgrid-gallery' => array(
+				'key' => 'gallery-wc-canvas',
+				'file' => 'boldgrid-gallery/wc-gallery.php',
+			),
+			'boldgrid-ninja-forms' => array(
+				'key' => 'ninja-forms',
+				'file' => 'boldgrid-ninja-forms/ninja-forms.php',
+			),
+		),
+	),
 );
