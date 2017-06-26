2302110e50995d096e33735fa10ddbf10b91ffec
Tim <dev@tim.ph>
2017-06-26 14:20:54 -0400

    removing old configs

--- src/library.config.php
+++ src/library.config.php
@@ -3,16 +3,9 @@
  * BoldGrid Premium Default Configurations.
  */
 
-$file = explode( '/', plugin_basename( __FILE__ ) );
-$slug = $file[0];
-
 return array(
 	'api' => 'https://api.boldgrid.com',
 	'option' => 'license',
-	'plugin_name' => $slug,
-	'plugin_path' => plugin_dir_path( "$slug/$slug.php" ),
-	'plugin_key_code' => $slug,
-	'main_file_path' => "$slug/$slug.php",
 	'key' => get_site_option( 'boldgrid_api_key', null ),
 	'apiData' => get_site_transient( 'boldgrid_api_data' ),
 
