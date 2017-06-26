6b1ee053b8b8b3538b49c07a2022d22c00cda5c8
Tim <dev@tim.ph>
2017-06-26 14:23:22 -0400

    adding release channel class

---
+++ src/Library/ReleaseChannel.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * BoldGrid Library Release Channel Class
+ *
+ * @package Boldgrid\Library
+ * @subpackage \Library
+ *
+ * @version 1.0.0
+ * @author BoldGrid <wpb@boldgrid.com>
+ */
+
+namespace Boldgrid\Library\Library;
+
+use Boldgrid\Library\Util;
+
+/**
+ * BoldGrid Library Release Channel Class.
+ *
+ * This class is responsible for retrieving and setting the release channel the
+ * user has selected for plugin and theme updates.
+ *
+ * @since 1.0.0
+ */
+class ReleaseChannel {
+
+	/**
+	 * @var string $pluginChannel The plugin release channel set for the user.
+	 * @var string $themeChannel  The theme release channel set for the user.
+	 */
+	private
+		$pluginChannel,
+		$themeChannel;
+
+	/**
+	 * Initialize class and set class properties.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		$this->setPluginChannel();
+		$this->setThemeChannel();
+	}
+
+	/**
+	 * Sets the pluginChannel class property.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return object $this Sets $pluginChannel class property.
+	 */
+	private function setPluginChannel() {
+		return $this->pluginChannel = Util\Option::get( 'release_channel' ) ? Util\Option::get( 'release_channel' ) : 'stable';
+	}
+
+	/**
+	 * Sets the themeChannel class property.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return object $this Sets $themeChannel class property.
+	 */
+	private function setThemeChannel() {
+		return $this->themeChannel = Util\Option::get( 'theme_release_channel' ) ? Util\Option::get( 'theme_release_channel' ) : 'stable';
+	}
+
+	/**
+	 * Gets the pluginChannel class property.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string $pluginChannel Plugin Channel that is set.
+	 */
+	public function getPluginChannel() {
+		return $this->pluginChannel;
+	}
+
+	/**
+	 * Gets the themeChannel class property.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string $themeChannel Theme Channel that is set.
+	 */
+	public function getThemeChannel() {
+		return $this->themeChannel;
+	}
+}
