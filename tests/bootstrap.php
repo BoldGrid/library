5546d20094fbad5cda1b56a3945fa1f5c73c3560
BradM <bradm@inmotionhosting.com>
2017-08-02 14:37:30 -0400

    TravisCI setup

---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
+
+require_once $_tests_dir . '/includes/functions.php';
+
+require_once dirname( dirname( __FILE__ ) ) . '/src/Library/License.php';
+require_once dirname( dirname( __FILE__ ) ) . '/src/Library/Api/Call.php';
+require_once dirname( dirname( __FILE__ ) ) . '/src/Library/Api/Availability.php';
+require_once dirname( dirname( __FILE__ ) ) . '/src/Library/Configs.php';
+require_once dirname( dirname( __FILE__ ) ) . '/src/Library/Filter.php';
+require_once dirname( dirname( __FILE__ ) ) . '/src/Library/Reseller.php';
+
+require $_tests_dir . '/includes/bootstrap.php';
---
+++ tests/test-reseller.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * BoldGrid Source Code
+ *
+ * @package Boldgrid_Plugintest
+ * @copyright BoldGrid.com
+ * @version $Id$
+ * @author BoldGrid.com <wpb@boldgrid.com>
+ */
+
+/**
+ * BoldGrid Library Reseller Test class.
+ *
+ * @since
+ */
+class Test_BoldGrid_Libarary_Reseller extends WP_UnitTestCase {
+
+	/**
+	 * Test default reseller data.
+	 */
+	public function testDefaults() {
+		$coinUrl = 'https://boldgrid.com/reseller-coin-url';
+
+		delete_option( 'boldgrid_reseller' );
+
+		$reseller = new Boldgrid\Library\Library\Reseller();
+
+		// With no reseller data, check that the defaults are being returned.
+		$this->assertSame( $reseller->centralUrl, $reseller->data['reseller_coin_url'] );
+
+		update_option( 'boldgrid_reseller', array(
+			'reseller_coin_url' => $coinUrl,
+		));
+
+		/*
+		 * @todo The Reseller class is supposed to hook into update_option
+		 * boldgrid_reseller, but it is not playing well with phpunit. Don't
+		 * rely on that filter to run, manually reset the data.
+		 */
+		$reseller->setData();
+
+		// With reseller data set, ensure defaults are not returned.
+		$this->assertSame( $coinUrl, $reseller->data['reseller_coin_url'] );
+	}
+}
