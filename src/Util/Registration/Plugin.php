49507011a12044c481cd48ab68b38bf72c52aa9a
BradM <bradm@inmotionhosting.com>
2018-02-22 15:34:09 -0500

    BGBKUP-147 Class 'Boldgrid\Library\Library\License' not found Premium/Attribution.php

--- src/Util/Registration/Plugin.php
+++ src/Util/Registration/Plugin.php
@@ -34,5 +34,46 @@ class Plugin extends Util\Registration {
 		parent::init( $product );
 		register_activation_hook( $this->getProduct(), array( $this, 'register' ) );
 		register_deactivation_hook( $this->getProduct(), array( $this, 'deregister' ) );
+
+		add_filter( 'upgrader_package_options', array( $this, 'preUpgrade' ) );
+	}
+
+	/**
+	 * Take action before this plugin is upgraded.
+	 *
+	 * It's nice that WordPress has an "upgrader_process_complete" action you
+	 * can hook into to take action after a plugin is upgraded. However, what
+	 * about an action BEFORE the upgrade? Such an action does not exist.
+	 *
+	 * What does exist is the ability to filter our $upgrader options before the
+	 * upgrade takes place. So, in order to do an action before the upgrade
+	 * runs, we'll hook into the upgrader_package_options filter.
+	 *
+	 * @since 2.2.1
+	 *
+	 * @param  array $options See filter declaration in
+	 *                        wp-admin/includes/class-wp-upgrader.php
+	 * @return array
+	 */
+	public function preUpgrade( $options ) {
+		$plugin = ! empty( $options['hook_extra']['plugin'] ) ? $options['hook_extra']['plugin'] : null;
+
+		if( $plugin === $this->getProduct() ) {
+
+			/*
+			 * Before this plugin is upgraded, remove it from the list of
+			 * registered libraries.
+			 *
+			 * We need to remove it because this plugin may be upgraded
+			 * (or downgraded) to a version of the plugin where it does not
+			 * contain the library.
+			 *
+			 * Reregistration of this plugin will take place in the constructor
+			 * the next time this class is instantiated.
+			 */
+			$this->deregister();
+		}
+
+		return $options;
 	}
 }
