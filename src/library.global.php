56bb8ae841c9b0cd3de683528926926e398715a5
Rafael Ramos <rramo012@gmail.com>
2018-05-24 13:23:21 -0400

    Update library.global.php

--- src/library.global.php
+++ src/library.global.php
@@ -65,7 +65,7 @@ return array(
 			),
 			'boldgrid-backup' => array(
 				'key' => 'backup',
-				'file' => 'boldgrid-backup/boldgrid-backup.php',
+				'file' => '//wordpress.org/plugins/boldgrid-backup/',
 				'priority' => 40,
 			),
 			'wpforms-lite' => array(
