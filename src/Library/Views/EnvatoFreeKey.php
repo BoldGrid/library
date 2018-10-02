7c26c11a55fd66e44a5447c301c7e83ee04c33c8
bradm@inmotionhosting.com <bradm@inmotionhosting.com>
2018-10-02 16:00:56 -0400

    Adding text domain

--- src/Library/Views/EnvatoFreeKey.php
+++ src/Library/Views/EnvatoFreeKey.php
@@ -1,12 +1,12 @@
 <!-- User has free key entered and should claim their free key -->
 <div class="envato-claim-message">
 	<p>
-		<?php _e( 'Thank you for your Envato Market purchase. You currently have a Free Connect Key entered,
-		but your Envato purchase entitles you to a Premium Connect Key.' ) ?>
+		<?php esc_html_e( 'Thank you for your Envato Market purchase. You currently have a Free Connect Key entered,
+		but your Envato purchase entitles you to a Premium Connect Key.', 'boldgrid-connect' ) ?>
 	</p>
 	<p>
 		<?php printf( __( 'Please visit %sBoldGrid Central%s
-		to link your accounts and claim your Premium Connect Key.' ),
+		to link your accounts and claim your Premium Connect Key.', 'boldgrid-connect' ),
 		'<a target="_blank" href="https://www.boldgrid.com/central/code/envato">',
 		'</a>' ) ?>
 	</p>
