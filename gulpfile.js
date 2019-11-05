08c4a44b2effd39097f5641f06a592f306fb265a
Joe Cartonia <joe@css-solutions.com>
2019-11-05 15:19:07 -0500

    Removed gulp and made building nice for Windows with yarn.

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require( 'gulp' );
-
-gulp.task( 'build', function() {
-	gulp.src( [ 'node_modules/jquery-toggles/toggles.min.js' ] ).pipe( gulp.dest( './build' ) );
-	gulp.src( [ 'node_modules/jquery-toggles/css/toggles-full.css' ] ).pipe( gulp.dest( './build' ) );
-} );
-
-gulp.task( 'default', [ 'build' ] );
