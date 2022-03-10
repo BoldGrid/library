a8af80bd4faa7247ae9d36bbb23fa53f13224cff
bradm@inmotionhosting.com <bradm@inmotionhosting.com>
2022-03-10 13:31:08 -0500

    Fixing build

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -45,5 +45,14 @@ require_once dirname( dirname( __FILE__ ) ) . '/src/Library/Activity.php';
 require_once dirname( dirname( __FILE__ ) ) . '/src/Util/Option.php';
 require_once dirname( dirname( __FILE__ ) ) . '/src/Util/Version.php';
 
-require $_tests_dir . '/includes/bootstrap.php';
+/*
+ * Yoast/PHPUnit-Polyfills, required for running the WP test suite.
+ * Please see https://make.wordpress.org/core/2021/09/27/changes-to-the-wordpress-core-php-test-suite/
+ *
+ * The WP Core test suite can now run on all PHPUnit versions between PHPUnit 5.7.21 up to the latest
+ * release (at the time of writing: PHPUnit 9.5.10), which allows for running the test suite against
+ * all supported PHP versions using the most appropriate PHPUnit version for that PHP version.
+ */
+require_once dirname( dirname( __FILE__ ) ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
 
+require $_tests_dir . '/includes/bootstrap.php';
